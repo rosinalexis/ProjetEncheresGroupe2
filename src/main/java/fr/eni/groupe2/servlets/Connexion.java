daf28969fa84ccedda0376b5de8a4f65852be533
Florian <34481286+OnlySKZ@users.noreply.github.com>
2021-04-08 14:52:53 +0200

    Fix erreur servlet connexion

--- src/main/java/fr/eni/groupe2/servlets/Connexion.java
+++ src/main/java/fr/eni/groupe2/servlets/Connexion.java
@@ -9,12 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bo.Utilisateur;
-import dal.DAOContext;
-import dal.UtilisateurDAO;
+import fr.eni.groupe2.bo.Utilisateur;
+import  fr.eni.groupe2.dal.jdbc.UtilisateurDAOJdbcImpl;
 
 @WebServlet(urlPatterns="/connexion", loadOnStartup = 1)
-public class connexion extends HttpServlet {
+public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
@@ -33,7 +32,7 @@ public class connexion extends HttpServlet {
 		request.setAttribute("login", login);
 		request.setAttribute("password", password);
 		
-		Utilisateur utilisateurConnecter = UtilisateurDAO.validerConnection(login, password);
+		Utilisateur utilisateurConnecter = UtilisateurDAOJdbcImpl.validerConnection(login, password);
 		if(utilisateurConnecter != null) {
 			HttpSession session = request.getSession(true);
 			session.setAttribute("utilisateurConnecter", utilisateurConnecter);
